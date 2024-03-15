d4342c22ae99d6ece599acc594d144a1db266956
Moises Laguna <lagunamoises64@gmail.com>
2024-03-15 14:48:22 -0600

    creacion de rutas de usuarios

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getUsers } from "../controlller/controllers.js";
+import { getUsers, createUser, getUser, deleteUser, updateUser } from "../controlller/controllers.js";
 
 const router = Router();
 
 router.get("/users", getUsers);
+router.get("/users/:id", getUser);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
